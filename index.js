541a254bc2fa602d965a9053bddc0b12769ca355
Eddie bautista <ebautistab@uniguajira.edu.co>
2024-05-02 09:52:37 -0500

    ajuste hola mundo

--- index.js
+++ index.js
@@ -2,7 +2,7 @@ const http = require('http');
 
 const server = http.createServer((req, res) => {
   res.writeHead(200, {'Content-Type': 'text/plain'});
-  res.end('Hola Eddie bautista de unir\n');
+  res.end('Hola Mundo, por Eddie bautista de unir \n');
 });
 
 const PORT = 3000;
