9e45b0dba433ff29487d9416fcc32e069254fe50
eddie bautista <ebautistab@uniguajira.edu.co>
2024-06-02 20:06:13 -0500

    Update index.js

--- index.js
+++ index.js
@@ -1,8 +1,32 @@
 const http = require('http');
+const { MongoClient } = require('mongodb');
+
+const MONGODB_IP = process.env.MONGODB_IP || 'localhost'; // Usar la IP de MongoDB desde las variables de entorno
+
+const url = `mongodb://${MONGODB_IP}:27017`;
+const dbName = 'test'; // Nombre de la base de datos a la que te quieres conectar
+
+const client = new MongoClient(url, { useNewUrlParser: true, useUnifiedTopology: true });
+
+let connectionMessage = 'Conexión no establecida a MongoDB';
+
+client.connect(err => {
+  if (err) {
+    console.error('Error al conectarse a MongoDB:', err);
+  } else {
+    console.log('Conexión exitosa a MongoDB');
+    connectionMessage = 'Conexión exitosa a MongoDB';
+  }
+
+  // Cerrar la conexión después de 5 segundos
+  setTimeout(() => {
+    client.close();
+  }, 5000);
+});
 
 const server = http.createServer((req, res) => {
-  res.writeHead(200, {'Content-Type': 'text/plain'});
-  res.end('Hola Mundo, por Eddie bautista de unir \n Url del mongo db es: '+`mongodb://${node.env.MONGODB_IP}:27017/`);
+  res.writeHead(200, { 'Content-Type': 'text/plain' });
+  res.end(`Hola Mundo, por Eddie Bautista de UNIR \nURL de MongoDB es: ${url}\n${connectionMessage}`);
 });
 
 const PORT = 3000;
